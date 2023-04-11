0b9a4f566b7a76ccae0b899837385aa41f24c064
Anthony <denayeranthony1782@gmail.com>
2023-04-11 20:49:00 +0200

    Ex fini

--- 3.Selectors/script.js
+++ 3.Selectors/script.js
@@ -1,23 +1,3 @@
-// document.body.onload = addElement();
-
-// function addElement() {
-//   let newArticle = document.createElement("article");
-//   let newSection = document.createElement("section");
-//   newArticle.appendChil(section);
-//   let addText = document.createTextNode("Hello");
-//   newArticle.appendChild(addText);
-
-//   let currentArticle = document.getElementById("article1");
-//   document.body.insertBefore(newArticle, currentArticle);
-// }
-let article = document.getElementsByClassName("article");
-let newSection = document.createElement("section");
-newSection.innerText = "Hello";
-// article.appendChild(newSection);
-let currentSection = document.getElementById("section1");
-document.body.insertBefore(newSection,article)
-
-
 let learners = [
   "Abel",
   "Alexandra",
@@ -41,4 +21,54 @@ let learners = [
   "Willy",
 ];
 
-function randomLearner() {}
+function addBackground(section) {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  section.style.backgroundColor = `rgb(${red} ${green} ${blue})
+  `;
+  if (red * 0.299 + green * 0.587 + blue * 0.114 > 186) {
+    section.style.color = "#000";
+  } else {
+    section.style.color = "#fff";
+  }
+}
+
+// if (red*0.299 + green*0.587 + blue*0.114) > 186 use #000000 else use #ffffff
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+
+let article = document.getElementsByTagName("article")[0];
+ console.log(article);
+function addLearners() {
+  let shuffledLearners = shuffleArray(learners);
+  for (i = 0; i < learners.length; i++) {
+    let learner = shuffledLearners[i];
+    let newSection = document.createElement("section");
+    let text = document.createTextNode(learner);
+    newSection.appendChild(text);
+    article.appendChild(newSection);
+    addBackground(newSection);
+  }
+}
+
+addLearners();
+
+// Math.floor(Math.random() * learners.length);
+
+//const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+//paragraph.style.color=randomColor;
+
+// document.createElement();
+// document.createTextNode();
+// .appendChild();
+// .insertBefore();
